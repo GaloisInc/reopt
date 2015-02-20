1f105a31d4aeddc3dfce60b978493a2d0ce9fb67
Simon Winwood <simonjwinwood@gmail.com>
2015-02-19 18:14:42 -0800

    Fiddled with generate_init_instructions.sh

--- generate_init_instructions.sh
+++ generate_init_instructions.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gobjdump -d examples/nweb23_static_fresbsd | sed -e 's/.\{32\}//' | cut -f 1 -d ' ' | sed -e 's/.*://' | sed -e 's/0.*//' | sort | uniq
+objdump -w --disassembler-options=intel-mnemonic -d examples/nweb23_static_fresbsd \
+    | cut -f 3 | sed -e 's/.*://' | cut -f1 -d ' ' | sort | uniq
