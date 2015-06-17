c010621b99992926a00f9e54735cd425ec0941f2
Nathan Collins <conathan@galois.com>
2015-06-16 19:13:01 -0700

    Fix imports for Simon's reorg and import qualified.

--- src/Reopt/Semantics/Concrete.hs
+++ src/Reopt/Semantics/Concrete.hs
@@ -10,12 +10,15 @@
 -- Reopt.Semantics.Monad that treat some class methods as
 -- uninterpreted functions.
 ------------------------------------------------------------------------
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE KindSignatures #-} -- MaybeF
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE RankNTypes #-}
@@ -24,14 +27,14 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE KindSignatures #-} -- MaybeF
 
 module Reopt.Semantics.Concrete
        (
        ) where
 
-import           Control.Applicative
+#if !MIN_VERSION_base(4,8,0)
+import Control.Applicative ((<*>), pure, Applicative)
+#endif
 import           Control.Exception (assert)
 import           Control.Lens
 import           Control.Monad.Cont
@@ -57,7 +60,7 @@ import           Numeric (showHex)
 import           Text.PrettyPrint.ANSI.Leijen (text, colon, (<>), (<+>))
 
 import qualified Flexdis86 as Flexdis
-import           Reopt.Memory
+import           Reopt.Object.Memory
 import           Reopt.Semantics.FlexdisMatcher (execInstruction)
 import           Reopt.Semantics.Monad
   ( Type(..)
@@ -66,9 +69,9 @@ import           Reopt.Semantics.Monad
   , bvLit
   )
 import qualified Reopt.Semantics.Monad as S
-import           Reopt.Semantics.Representation
-import qualified Reopt.Semantics.StateNames as N
-import           Reopt.Semantics.Types (TypeBits)
+import qualified Reopt.CFG.Representation as R
+import qualified Reopt.Machine.StateNames as N
+import           Reopt.Machine.Types (type_width, FloatInfo(..), TypeBits)
 
 ------------------------------------------------------------------------
 -- Expr
@@ -88,9 +91,9 @@ import           Reopt.Semantics.Types (TypeBits)
 -- | A pure expression for isValue.
 data Expr tp where
   -- An expression obtained from some value.
-  ValueExpr :: !(Value tp) -> Expr tp
+  LitExpr :: !(NatRepr n) -> Integer -> Expr (BVType n)
   -- An expression that is computed from evaluating subexpressions.
-  AppExpr :: !(App Expr tp) -> Expr tp
+  AppExpr :: !(R.App Expr tp) -> Expr tp
 
   -- Extra constructors where 'App' does not provide what we want.
   LowerHalfExpr :: (1 <= n) =>
@@ -100,7 +103,7 @@ data Expr tp where
   -- Here 'App' has 'Trunc', but its type is different; see notes at
   -- bottom of file.
   TruncExpr :: (1 <= m, m <= n) =>
-    !(NatRepr m) -> Expr (BVType n) -> Expr (BVType m)
+    !(NatRepr m) -> !(Expr (BVType n)) -> Expr (BVType m)
   -- Here 'app' has 'SExt', but its type is different as with 'Trunc'.
   -- But, strangely, the strict version of 'uext' is in the 'IsValue'
   -- class as 'uext'', so we can use 'App's 'UExt' there ... seems ad
@@ -108,12 +111,16 @@ data Expr tp where
   SExtExpr :: (1 <= m, m <= n) =>
     !(NatRepr n) -> Expr (BVType m) -> Expr (BVType n)
 
-app :: App Expr tp -> Expr tp
+mkLit :: NatRepr n -> Integer -> Value (BVType n)
+mkLit n v = LitExpr n (v .&. mask)
+  where mask = maxUnsigned n
+
+app :: R.App Expr tp -> Expr tp
 app = AppExpr
 
 exprType :: Expr tp -> S.TypeRepr tp
-exprType (ValueExpr v) = valueType v
-exprType (AppExpr a) = appType a
+exprType (LitExpr r _) = S.BVTypeRepr r
+exprType (AppExpr a) = R.appType a
 -- Added constructors for lower and upper half to avoid duplicating a
 -- bunch of code in 'Reopt.Semantics.Implementation'.
 exprType (LowerHalfExpr r _) = S.BVTypeRepr $ halfNat r
@@ -132,49 +139,49 @@ exprWidth e =
 -- corresponding constructors, or add them to 'Expr' above.
 instance S.IsValue Expr where
   bv_width = exprWidth
-  mux c x y = app $ Mux (exprWidth x) c x y
-  bvLit n v = ValueExpr $ mkLit n (toInteger v)
-  bvAdd x y = app $ BVAdd (exprWidth x) x y
-  bvSub x y = app $ BVSub (exprWidth x) x y
-  bvMul x y = app $ BVMul (exprWidth x) x y
-  complement x = app $ BVComplement (exprWidth x) x
-  x .&. y = app $ BVAnd (exprWidth x) x y
-  x .|. y = app $ BVOr (exprWidth x) x y
-  bvXor x y = app $ BVXor (exprWidth x) x y
-  x .=. y = app $ BVEq x y
+  mux c x y = app $ R.Mux (exprWidth x) c x y
+  bvLit n v = mkLit n (toInteger v)
+  bvAdd x y = app $ R.BVAdd (exprWidth x) x y
+  bvSub x y = app $ R.BVSub (exprWidth x) x y
+  bvMul x y = app $ R.BVMul (exprWidth x) x y
+  complement x = app $ R.BVComplement (exprWidth x) x
+  x .&. y = app $ R.BVAnd (exprWidth x) x y
+  x .|. y = app $ R.BVOr (exprWidth x) x y
+  bvXor x y = app $ R.BVXor (exprWidth x) x y
+  x .=. y = app $ R.BVEq x y
   bvSplit v = (UpperHalfExpr (exprWidth v) v, LowerHalfExpr (exprWidth v) v)
-  bvShr x y = app $ BVShr (exprWidth x) x y
-  bvSar x y = app $ BVSar (exprWidth x) x y
-  bvShl x y = app $ BVShl (exprWidth x) x y
+  bvShr x y = app $ R.BVShr (exprWidth x) x y
+  bvSar x y = app $ R.BVSar (exprWidth x) x y
+  bvShl x y = app $ R.BVShl (exprWidth x) x y
   bvTrunc w x = TruncExpr w x
-  bvUlt x y = app $ BVUnsignedLt x y
-  bvSlt x y = app $ BVUnsignedLt x y
-  bvBit x y = app $ BVBit x y
+  bvUlt x y = app $ R.BVUnsignedLt x y
+  bvSlt x y = app $ R.BVUnsignedLt x y
+  bvBit x y = app $ R.BVBit x y
   sext w x = SExtExpr w x
-  uext' w x = app $ UExt x w
-  even_parity x = app $ EvenParity x
-  reverse_bytes x = app $ ReverseBytes (exprWidth x) x
-  uadc_overflows x y c = app $ UadcOverflows (exprWidth x) x y c
-  sadc_overflows x y c = app $ SadcOverflows (exprWidth x) x y c
-  usbb_overflows x y c = app $ UsbbOverflows (exprWidth x) x y c
-  ssbb_overflows x y c = app $ SsbbOverflows (exprWidth x) x y c
-  bsf x = app $ Bsf (exprWidth x) x
-  bsr x = app $ Bsr (exprWidth x) x
-  isQNaN rep x = app $ FPIsQNaN rep x
-  isSNaN rep x = app $ FPIsSNaN rep x
-  fpAdd rep x y = app $ FPAdd rep x y
-  fpAddRoundedUp rep x y = app $ FPAddRoundedUp rep x y
-  fpSub rep x y = app $ FPSub rep x y
-  fpSubRoundedUp rep x y = app $ FPSubRoundedUp rep x y
-  fpMul rep x y = app $ FPMul rep x y
-  fpMulRoundedUp rep x y = app $ FPMulRoundedUp rep x y
-  fpDiv rep x y = app $ FPDiv rep x y
-  fpLt rep x y = app $ FPLt rep x y
-  fpEq rep x y = app $ FPEq rep x y
-  fpCvt src tgt x = app $ FPCvt src x tgt
-  fpCvtRoundsUp src tgt x = app $ FPCvtRoundsUp src x tgt
-  fpFromBV tgt x = app $ FPFromBV x tgt
-  truncFPToSignedBV tgt src x = app $ TruncFPToSignedBV src x tgt
+  uext' w x = app $ R.UExt x w
+  even_parity x = app $ R.EvenParity x
+  reverse_bytes x = app $ R.ReverseBytes (exprWidth x) x
+  uadc_overflows x y c = app $ R.UadcOverflows (exprWidth x) x y c
+  sadc_overflows x y c = app $ R.SadcOverflows (exprWidth x) x y c
+  usbb_overflows x y c = app $ R.UsbbOverflows (exprWidth x) x y c
+  ssbb_overflows x y c = app $ R.SsbbOverflows (exprWidth x) x y c
+  bsf x = app $ R.Bsf (exprWidth x) x
+  bsr x = app $ R.Bsr (exprWidth x) x
+  isQNaN rep x = app $ R.FPIsQNaN rep x
+  isSNaN rep x = app $ R.FPIsSNaN rep x
+  fpAdd rep x y = app $ R.FPAdd rep x y
+  fpAddRoundedUp rep x y = app $ R.FPAddRoundedUp rep x y
+  fpSub rep x y = app $ R.FPSub rep x y
+  fpSubRoundedUp rep x y = app $ R.FPSubRoundedUp rep x y
+  fpMul rep x y = app $ R.FPMul rep x y
+  fpMulRoundedUp rep x y = app $ R.FPMulRoundedUp rep x y
+  fpDiv rep x y = app $ R.FPDiv rep x y
+  fpLt rep x y = app $ R.FPLt rep x y
+  fpEq rep x y = app $ R.FPEq rep x y
+  fpCvt src tgt x = app $ R.FPCvt src x tgt
+  fpCvtRoundsUp src tgt x = app $ R.FPCvtRoundsUp src x tgt
+  fpFromBV tgt x = app $ R.FPFromBV x tgt
+  truncFPToSignedBV tgt src x = app $ R.TruncFPToSignedBV src x tgt
 
 -- ??? Why do the 'App' constructors take a 'NatRepr' argument? It can
 -- always be reconstructed by the user using 'bv_width' after
