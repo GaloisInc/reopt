7f39e58637ffc4906bdeb9f6244ee679fd98830b
Nathan Collins <conathan@galois.com>
2015-06-23 21:54:09 -0700

    Define operations on `Value`s.
    These lift the operations on `BitVector`s from the last commit. But we
    have to manually compute the return type in the case of `Undefined`
    inputs, so there's a bit of duplicated work.

--- src/Reopt/Semantics/ConcreteState.hs
+++ src/Reopt/Semantics/ConcreteState.hs
@@ -1,22 +1,25 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE ViewPatterns #-}
-
 
 module Reopt.Semantics.ConcreteState
     ( module Reopt.Semantics.ConcreteState
     , module Reopt.Semantics.BitVector
     ) where
 
-import           Data.BitVector (BV)
+import qualified Data.BitVector as BV
+import           Data.Map (Map)
+import           Data.Maybe (mapMaybe)
 import           Text.PrettyPrint.ANSI.Leijen ((<+>), Pretty(..), text)
 
 import           Data.Parameterized.NatRepr
 import qualified Reopt.Machine.StateNames as N
 import           Reopt.Machine.Types
-import           Reopt.Machine.X86State
-import           Reopt.Semantics.BitVector
+import qualified Reopt.Machine.X86State as X
+import           Reopt.Semantics.BitVector (BitVector, BV, bitVector, unBitVector)
+import qualified Reopt.Semantics.BitVector as B
 
 ------------------------------------------------------------------------
 -- Concrete values
@@ -37,7 +40,7 @@ instance Pretty (Value tp) where
   pretty (Literal x)    = text $ show x
   pretty (Undefined _) = text $ "Undefined"
 
-instance PrettyRegValue Value where
+instance X.PrettyRegValue Value where
   ppValueEq (N.FlagReg n) _ | not (n `elem` [0,2,4,6,7,8,9,10,11]) = Nothing
   ppValueEq (N.X87ControlReg n) _ | not (n `elem` [0,1,2,3,4,5,12]) = Nothing
   ppValueEq r v = Just $ text (show r) <+> text "=" <+> pretty v
@@ -69,4 +72,35 @@ liftValue2 _ nr _ _ = Undefined (BVTypeRepr nr)
 asBV :: Value tp -> Maybe BV
 asBV (Literal (unBitVector -> (_, bv))) = Just bv
 asBV _ = Nothing
+
+------------------------------------------------------------------------
+-- Operations on 'Value's
+
+width :: Value (BVType n) -> NatRepr n
+width (Literal bv) = B.width bv
+width (Undefined tr) = type_width tr
+
+-- | Concatenate two 'Value's.
+(#) :: Value (BVType n1) -> Value (BVType n2) -> Value (BVType (n1 + n2))
+Literal b1 # Literal b2 = Literal (b1 B.# b2)
+v1 # v2 = Undefined (BVTypeRepr $ addNat (width v1) (width v2))
+
+-- | Group a 'Value' in size 'n1' chunks.
+--
+-- If 'n1' does not divide 'n2', then the first chunk will be
+-- zero-extended.
+group :: NatRepr n1 -> Value (BVType n2) -> [Value (BVType n1)]
+group nr (Literal b) = [ Literal b' | b' <- B.group nr b ]
+group nr v@(Undefined _) = replicate count (Undefined (BVTypeRepr nr))
+  where
+    -- | The ceiling of @n2 / n1@.
+    count = fromIntegral $
+      (natValue (width v) + natValue nr - 1) `div` natValue nr
+
+-- | Modify the underlying 'BV'.
+--
+-- The modification must not change the width.
+modify :: (BV -> BV) -> Value (BVType n) -> Value (BVType n)
+modify f (Literal b) = Literal (B.modify f b)
+modify _ v@(Undefined _) = v
 
