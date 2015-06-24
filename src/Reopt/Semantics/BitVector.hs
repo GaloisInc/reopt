9708a5abf6f251a4d27d2a1e013b645438bf4aa7
Nathan Collins <conathan@galois.com>
2015-06-23 21:49:42 -0700

    Define operations on `BitVector`s.
    I defined these so that the corresponding operations on
    `Reopt.Semantics.ConcreteValue.Value`s could be defined in terms of
    these. However, because we need to independently compute the result
    type there in case of any `Undefined`s, these functions represent a
    bit of duplicated work: we could probablyu just as well use
    `Reopt.Semantics.ConcreteValue.liftValue2` or similar combinators and
    directly manipulate `BV`s there.

--- src/Reopt/Semantics/BitVector.hs
+++ src/Reopt/Semantics/BitVector.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Reopt.Semantics.BitVector
   (
@@ -8,7 +9,13 @@ module Reopt.Semantics.BitVector
     BitVector
   , bitVector
   , unBitVector
+  -- | Operations.
+  , (#)
+  , group
+  , modify
+  , width
   -- | To construct 'BV.BV' values.
+  , BV
   , BV.bitVec
   ) where
 
@@ -47,3 +54,28 @@ instance Eq (BitVector n) where
 
 instance Show (BitVector n) where
   show (BitVector _ x) = show x
+
+------------------------------------------------------------------------
+-- Operations on 'BitVector's
+
+width :: BitVector n -> NatRepr n
+width (BitVector nr _) = nr
+
+-- | Concatenate.
+(#) :: BitVector n1 -> BitVector n2 -> BitVector (n1 + n2)
+(BitVector nr1 bv1) # (BitVector nr2 bv2) =
+  bitVector (addNat nr1 nr2) (bv1 BV.# bv2)
+
+-- | Group into chunks of 'n1' bits.
+--
+-- If 'n1' does not divide 'n2', then the first chunk will be
+-- zero-extended.
+group :: NatRepr n1 -> BitVector n2 -> [BitVector n1]
+group nr (BitVector _ bv) =
+  [ bitVector nr bv' | bv' <- BV.group (natValue nr) bv ]
+
+-- | Modify the underlying 'BV'.
+--
+-- The modification must not change the width.
+modify :: (BV -> BV) -> BitVector n -> BitVector n
+modify f (BitVector nr bv) = bitVector nr (f bv)
