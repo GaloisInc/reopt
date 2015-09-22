fc0a309ec6bd0a8f5a97bef9938741d06015625d
Ben Davis <ben@galois.com>
2015-09-22 18:13:42 +0000

    add xor no-function-call bins

---
+++ examples/tests/xor-nof.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main (int argc, char ** argv) {
+  unsigned int x = 0x12345678, y = 0x23456789, z = 0x34567890, w = 0x4567890A;
+
+  int count = argc;
+
+  for (int i = 0; i < count; i++) {
+    unsigned int t = x ^ (x<<11);
+    x = y;
+    y = z;
+    z = w;
+    w = w ^ (w >> 19) ^ t ^ (t >> 8);
+  }
+
+  return w;
+}
