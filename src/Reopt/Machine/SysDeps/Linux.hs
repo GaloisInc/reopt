28b8209c933f9a1f8f66d868b0a8c6351a2f0f28
Simon Winwood <simonjwinwood@gmail.com>
2016-02-17 18:22:40 -0800

    Added syscall tables for Linux

---
+++ src/Reopt/Machine/SysDeps/Linux.hs
@@ -0,0 +1,18 @@
+
+module Reopt.Machine.SysDeps.Linux
+       ( syscallTypeInfo
+       , SyscallArgType (..)
+       , module Reopt.Machine.SysDeps.Types
+       ) where
+
+import qualified Data.Map as Map
+import           Data.Word
+
+import           Reopt.Machine.SysDeps.LinuxGenerated (syscallInfo)
+import           Reopt.Machine.SysDeps.Types
+
+-- We might want to export more later, hence we export this function
+-- instead of just the map
+syscallTypeInfo :: Word64 -> Maybe SyscallTypeInfo
+syscallTypeInfo k = Map.lookup k syscallInfo
+
