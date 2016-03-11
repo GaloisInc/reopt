b766250410ebbfb0c8849d67f2b034c6f53679db
Simon Winwood <simonjwinwood@gmail.com>
2016-03-11 13:32:40 -0800

    Fixed bug with rol/ror
    In the case that the amount to rotate by is zero, the target register is
    still modified.  This is important for 32 bit ops as they zero extend.

--- src/Reopt/Semantics/Semantics.hs
+++ src/Reopt/Semantics/Semantics.hs
@@ -706,16 +706,20 @@ exec_rol l count = do
       r = bvRol v effective
 
   -- When the count is zero, nothing happens, in particular, no flags change
-  when_ (complement $ is_zero low_count) $ do
-    let new_cf = bvBit r (bvLit (bv_width r) (0 :: Int))
-
-    cf_loc .= new_cf
-
-    ifte_ (low_count .=. bvLit (bv_width low_count) (1 :: Int))
-      (of_loc .= (msb r `bvXor` new_cf))
-      (set_undefined of_loc)
-
-    l .= r
+  ifte_ (complement $ is_zero low_count)
+        -- In the case of a zero count, we still do the assignment.
+        -- This is relevant only for 32 bit ops where this will
+        -- zero-extend.
+    (l .= v)
+    $ do let new_cf = bvBit r (bvLit (bv_width r) (0 :: Int))
+      
+         cf_loc .= new_cf
+      
+         ifte_ (low_count .=. bvLit (bv_width low_count) (1 :: Int))
+           (of_loc .= (msb r `bvXor` new_cf))
+           (set_undefined of_loc)
+      
+         l .= r
 
 -- FIXME: use really_exec_shift above?
 exec_ror :: (1 <= n', n' <= n, IsLocationBV m n)
@@ -739,16 +743,19 @@ exec_ror l count = do
       r = bvRor v effective
 
   -- When the count is zero, nothing happens, in particular, no flags change
-  when_ (complement $ is_zero low_count) $ do
-    let new_cf = bvBit r (bvLit (bv_width r) (widthVal (bv_width r) - 1))
-
-    cf_loc .= new_cf
-
-    ifte_ (low_count .=. bvLit (bv_width low_count) (1 :: Int))
-      (of_loc .= (msb r `bvXor` bvBit r (bvLit (bv_width r) (widthVal (bv_width v) - 2))))
-      (set_undefined of_loc)
-
-    l .= r
+  ifte_ (is_zero low_count) 
+        -- In the case of a zero count, we still do the assignment.
+        -- This is relevant only for 32 bit ops where this will
+        -- zero-extend.
+    (l .= v)
+    $ do let new_cf = bvBit r (bvLit (bv_width r) (widthVal (bv_width r) - 1))
+         cf_loc .= new_cf
+      
+         ifte_ (low_count .=. bvLit (bv_width low_count) (1 :: Int))
+           (of_loc .= (msb r `bvXor` bvBit r (bvLit (bv_width r) (widthVal (bv_width v) - 2))))
+           (set_undefined of_loc)
+      
+         l .= r
 
 
 -- ** Bit and Byte Instructions
