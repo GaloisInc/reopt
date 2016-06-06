e1dd18100a2f2d9192f38108ace6b207b760a231
Ben Davis <ben@galois.com>
2016-06-06 10:23:46 -0400

    Update build script: replace elf with elf-edit

--- update-deps.sh
+++ update-deps.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 set -e
 
-# Install reopt in a Cabal sandbox.
+# Clone and update dependencies
 
-PRIVATE_GITHUB_REPOS=(elf flexdis86 parameterized-utils linux-ptrace posix-waitpid mss llvm-pretty)
+PRIVATE_GITHUB_REPOS=(elf-edit flexdis86 parameterized-utils linux-ptrace posix-waitpid llvm-pretty fuzz64)
 cd "$(dirname "${BASH_SOURCE[0]}")"
 sandbox=$(pwd)/sandbox
 
