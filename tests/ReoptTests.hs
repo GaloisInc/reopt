ed06adb91d83398709a4af048095364334954914
Ben Selfridge <benselfridge@galois.com>
2017-12-04 10:12:25 -0800

    Working on reopt tests

--- tests/ReoptTests.hs
+++ tests/ReoptTests.hs
@@ -10,18 +10,32 @@ module ReoptTests (
 import qualified Control.Monad.Catch as C
 import qualified Data.ByteString as B
 import           Data.Typeable ( Typeable )
+import           System.FilePath.Posix
 import Test.Tasty as T
 import Test.Tasty.HUnit as T
 
 import qualified Data.ElfEdit as E
+import           Data.Macaw.Architecture.Info ( ArchitectureInfo(..) )
+import           Data.Macaw.Discovery
 import qualified Data.Macaw.Memory as MM
 import qualified Data.Macaw.Memory.ElfLoader as MM
+
+import           Reopt.Interface
 
 reoptTests :: [FilePath] -> T.TestTree
 reoptTests = T.testGroup "reopt" . map mkTest
 
 mkTest :: FilePath -> T.TestTree
-mkTest fp = T.testCase fp $ putStrLn "dummy test"
+mkTest fp = T.testCase fp $ withELF fp $ \e -> do
+  (secMap, mem) <- either fail return $ MM.memoryForElf loadOpts e
+  SomeArch ainfo <- getElfArchInfo e
+  let output_path = replaceFileName fp (takeBaseName fp ++ ".blocks")
+  E.elfClassInstances (E.elfClass e) $ do
+    withArchConstraints ainfo $ do
+      (disc_info,_) <- mkFinalCFGWithSyms ainfo mem e discOpts
+      writeFile output_path $ show $ ppDiscoveryStateBlocks disc_info
+  where loadOpts = undefined
+        discOpts = undefined
 
 withELF :: FilePath -> (E.Elf 64 -> IO ()) -> IO ()
 withELF fp k = do
