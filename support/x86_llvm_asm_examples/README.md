bb44bb526f16df919e4322d13befe3560d5578e2
Joe Hendrix <jhendrix@galois.com>
2018-07-24 12:51:53 -0700

    LLVM x86 movsd test

---
+++ support/x86_llvm_asm_examples/README.md
@@ -0,0 +1,5 @@
+This directory contains development artifacts used to understand how
+to create inline assembly in LLVM for x86 specific operations.
+
+Th example files will also let us test the semantics of certain tricky
+instructions.  To run these test, simply type `make`.
---
+++ support/x86_llvm_asm_examples/test_movsd.c
@@ -0,0 +1,165 @@
+// This file contains C using inline assembly for the movsd instruction.
+//
+// It is designex to both let us test the semantics of movsd, and provide
+// a way of viewing LLVM's inline assembly syntax.
+
+#include<stdint.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+////////////////////////////////////////////////////////////////////////
+// Common code
+
+static
+void ffail(const char* nm, const char* prop_name) {
+    fprintf(stderr, "%s: %s failed\n", nm, prop_name);
+    exit(1);
+}
+
+#define fail() ffail(__FUNCTION__, "bad")
+
+#define assert(nm, b) if (!(b)) { ffail(__FUNCTION__, nm); }
+
+#define arraylen(a) (sizeof(a) / sizeof(a[0]))
+
+////////////////////////////////////////////////////////////////////////
+// Primitives
+
+// These functions directly implement a rep_movsb program.
+
+// rep movsb with df=0
+__attribute__ ((noinline))
+void rep_movsb_df0(uint8_t** dest, uint8_t** src, uint64_t* count) {
+  asm ("cld\nrep movsb"
+       : "+c" (*count), "+S" (*src), "+D" (*dest));
+}
+
+__attribute__ ((noinline))
+void rep_movsw_df0(uint16_t** dest, uint16_t** src, uint64_t* count) {
+  asm ("cld\nrep movsw"
+       : "+c" (*count), "+S" (*src), "+D" (*dest));
+}
+
+__attribute__ ((noinline))
+void rep_movsd_df0(uint32_t** dest, uint32_t** src, uint64_t* count) {
+  asm ("cld\nrep movsd"
+       : "+c" (*count), "+S" (*src), "+D" (*dest));
+}
+
+__attribute__ ((noinline))
+void rep_movsq_df0(uint64_t** dest, uint64_t** src, uint64_t* count) {
+  asm ("cld\nrep movsq"
+       : "+c" (*count), "+S" (*src), "+D" (*dest));
+}
+
+////////////////////////////////////////////////////////////////////////
+// Tests
+
+// This tests the rep movsb implementation
+static
+void test_rep_movsb_df0() {
+    uint8_t src[] = {1,2};
+    uint8_t dest_init[] = {5,6,7,8};
+
+    uint8_t dest[arraylen(dest_init)];
+    memcpy(dest, dest_init, sizeof(dest_init));
+
+    uint8_t* dest_ptr = dest;
+    uint8_t* src_ptr  = src;
+    uint64_t cnt=arraylen(src);
+
+    rep_movsb_df0(&dest_ptr, &src_ptr, &cnt);
+
+    // Number of bytes not copied.
+    uint64_t left = sizeof(dest) - sizeof(src);
+
+    assert("check_dest", dest_ptr == dest + arraylen(src));
+    assert("check_src",  src_ptr  == src  + arraylen(src));
+    assert("check_cnt",  cnt == 0);
+    assert("mem_pre",  memcmp(src, dest, sizeof(src)) == 0);
+    assert("mem_post", memcmp(dest+arraylen(src), dest_init+arraylen(src), left) == 0);
+}
+
+// This tests the rep movsw implementation
+void test_rep_movsw_df0() {
+    uint16_t src[] = {1,2};
+    uint16_t dest_init[] = {5,6,7,8};
+
+    uint16_t dest[arraylen(dest_init)];
+    memcpy(dest, dest_init, sizeof(dest_init));
+
+    uint16_t* dest_ptr = dest;
+    uint16_t* src_ptr  = src;
+    uint64_t cnt=arraylen(src);
+
+    rep_movsw_df0(&dest_ptr, &src_ptr, &cnt);
+
+    // Number of bytes not copied.
+    uint64_t left = sizeof(dest) - sizeof(src);
+
+    assert("check_dest", dest_ptr == dest + arraylen(src));
+    assert("check_src",  src_ptr  == src  + arraylen(src));
+    assert("check_cnt",  cnt == 0);
+    assert("mem_pre",  memcmp(src, dest, sizeof(src)) == 0);
+    assert("mem_post", memcmp(dest+arraylen(src), dest_init+arraylen(src), left) == 0);
+}
+
+// This tests the rep movsd implementation
+void test_rep_movsd_df0() {
+    uint32_t src[] = {1,2};
+    uint32_t dest_init[] = {5,6,7,8};
+
+    uint32_t dest[arraylen(dest_init)];
+    memcpy(dest, dest_init, sizeof(dest_init));
+
+    uint32_t* dest_ptr = dest;
+    uint32_t* src_ptr  = src;
+    uint64_t cnt=arraylen(src);
+
+    rep_movsd_df0(&dest_ptr, &src_ptr, &cnt);
+
+    // Number of bytes not copied.
+    uint64_t left = sizeof(dest) - sizeof(src);
+
+    assert("check_dest", dest_ptr == dest + arraylen(src));
+    assert("check_src",  src_ptr  == src  + arraylen(src));
+    assert("check_cnt",  cnt == 0);
+    assert("mem_pre",  memcmp(src, dest, sizeof(src)) == 0);
+    assert("mem_post", memcmp(dest+arraylen(src), dest_init+arraylen(src), left) == 0);
+}
+
+// This tests the rep movsq implementation
+void test_rep_movsq_df0() {
+    uint64_t src[] = {1,2};
+    uint64_t dest_init[] = {5,6,7,8};
+
+    uint64_t dest[arraylen(dest_init)];
+    memcpy(dest, dest_init, sizeof(dest_init));
+
+    uint64_t* dest_ptr = dest;
+    uint64_t* src_ptr  = src;
+    uint64_t cnt=arraylen(src);
+
+    rep_movsq_df0(&dest_ptr, &src_ptr, &cnt);
+
+    // Number of bytes not copied.
+    uint64_t left = sizeof(dest) - sizeof(src);
+
+    assert("check_dest", dest_ptr == dest + arraylen(src));
+    assert("check_src",  src_ptr  == src  + arraylen(src));
+    assert("check_cnt",  cnt == 0);
+    assert("mem_pre",  memcmp(src, dest, sizeof(src)) == 0);
+    assert("mem_post", memcmp(dest+arraylen(src), dest_init+arraylen(src), left) == 0);
+}
+
+////////////////////////////////////////////////////////////////////////
+// main
+
+int main(int argc, char** argv) {
+    test_rep_movsb_df0();
+    test_rep_movsw_df0();
+    test_rep_movsd_df0();
+    test_rep_movsw_df0();
+    return 0;
+}
