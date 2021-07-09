4d576c40736501cd85c79613fac3ae79a015840c
Andrew Kent <andrew@galois.com>
2021-07-09 14:18:10 -0700

    chore: update benchmark libs and scripts related to them

--- scripts/test_reopt.sh
+++ scripts/test_reopt.sh
@@ -4,25 +4,25 @@ set -Eeuox pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-CENTOS7_DEV_DIR="$DIR/../deps/reopt-benchmark-binaries/centos7-dev/"
+CENTOS7_DEV_DIR="$DIR/../deps/reopt-benchmark-binaries/centos7-dev"
 
 /bin/bash $DIR/unpack_benchmarks.sh
 
 if [[ $# -lt 1 ]]; then
   echo "No binaries specified to test reopt against: running small default set of reopt tests..."
   pushd $CENTOS7_DEV_DIR
-  cabal run exe:reopt -- -c centos7-dev-bin/mkdir --lib-dir=centos7-dev-lib64 --debug-dir=centos7-dev-debug-lib64
-  cabal run exe:reopt -- -c centos7-dev-bin/curl --lib-dir=centos7-dev-lib64 --debug-dir=centos7-dev-debug-lib64
-  cabal run exe:reopt -- -c centos7-dev-bin/date --lib-dir=centos7-dev-lib64 --debug-dir=centos7-dev-debug-lib64
+  cabal run exe:reopt -- -c bin/mkdir --lib-dir=lib64 --debug-dir=debug-lib64
+  cabal run exe:reopt -- -c bin/curl --lib-dir=lib64 --debug-dir=debug-lib64
+  cabal run exe:reopt -- -c bin/date --lib-dir=lib64 --debug-dir=debug-lib64
   popd > /dev/null # $CENTOS7_DEV_DIR
 else
   pushd $CENTOS7_DEV_DIR
   for arg in "$@"
   do
-    if [[ -x "centos7-dev-bin/$arg" ]]; then
-      cabal run exe:reopt -- -c centos7-dev-bin/$arg --lib-dir=centos7-dev-lib64 --debug-dir=centos7-dev-debug-lib64
+    if [[ -x "bin/$arg" ]]; then
+      cabal run exe:reopt -- -c bin/$arg --lib-dir=lib64 --debug-dir=debug-lib64
     else
-      echo "Could not find binary $arg in centos7-dev-bin"
+      echo "Could not find binary $arg in bin"
       exit 1
     fi
   done
