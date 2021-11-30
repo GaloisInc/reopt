f43e0ec57437135028fcb37b1a62fa2d698132dd
Andrew Kent <andrew@galois.com>
2021-11-30 12:51:08 -0800

    simplify types and constraints

--- src/Reopt/TypeInference/Constraints.hs
+++ src/Reopt/TypeInference/Constraints.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 -- Loosely based on 'TIE: Principled Reverse Engineering of Types in Binary
 -- Programs' (NDSS 2011) by Jonghyup Lee, Thanassis Avgerinos, and David Brumley
 
@@ -5,52 +6,62 @@
 module Reopt.TypeInference.Constraints where
 
 
-
-data Ty
-  = TyData DataTy
-  | TyFun
-  | TyTop
-  | TyBot
-  | TyAnd Ty Ty
-  | TyOr Ty Ty
-  | TyArrow Ty Ty
-
-data DataTy
-  = DTyBase BaseTy
-  | DTyMem MemTy
+data NumSize
+  = NumSize8
+  | NumSize16
+  | NumSize32
+  | NumSize64
+  deriving (Eq, Ord)
 
 
-data BaseTy
-  = BTyBase RegTy
-  | BTyRefined RefinedTy
+newtype TyVar = TyVar {tyVarInt :: Int }
+  deriving (Eq, Ord)
 
--- | Maps each memory cell address to the type stored at that address.
-data MemTy = MemTy -- ??? {∀ addresses i|li : Ti } wut is that? a set of address/type mappings?
+-- | Types of values in x86 machine code
+data X86Ty
+  = UnknownTy -- the top type
+  | AbsurdTy -- the bottom type
+  | VarTy TyVar -- a type unification variable
+  | PtrTy
+  | NumTy NumSize
+  | ScalarFloatTy
+  | VecFloatTy
+  deriving (Eq, Ord)
+
+-- | x86 type constraints
+data X86TyConstraint
+  = -- | These types are known to be equal.
+    EqC X86Ty X86Ty
+  -- | These types were used in an addition operation of width @NumSize@.
+  | AddC NumSize X86Ty X86Ty
+
+numSizeInt :: NumSize -> Int
+numSizeInt =
+  \case
+    NumSize8 -> 8
+    NumSize16 -> 16
+    NumSize32 -> 32
+    NumSize64 -> 64
+
+-- | @subtype t1 t2@ returns @True@ iff @t1@ is a subtype of @t2@.
+subtype :: X86Ty -> X86Ty -> Bool
+subtype t1 t2 =
+  case (t1,t2) of
+    (_,_) | t1 == t2 -> True
+    (_, UnknownTy) -> True
+    (AbsurdTy, _) -> True
+    (NumTy n1, NumTy n2) -> (numSizeInt n1) <= (numSizeInt n2)
+    (_,_) -> False
 
 
 
--- | Register types, Cf. τ_reg
-data RegTy
-  = Reg1Ty
-  | Reg8Ty
-  | Reg16Ty
-  | Reg32Ty
-  | Reg64Ty
 
+-- -- | Calculates the least upper bound of two types (denoted by the “join”
+-- operator ⊔).
+-- join :: Ty -> Ty -> Ty
+-- join  = undefined -- FIXME
 
--- | Refined types, Cf. τ_refined
-data RefinedTy
-  = Num8RTy
-  | Num16RTy
-  | Num32RTy
-  | Num64RTy
-  | UInt8RTy
-  | UInt16RTy
-  | UInt32RTy
-  | UInt64RTy
-  | Int8RTy
-  | Int16RTy
-  | Int32RTy
-  | Int64RTy
-  | PtrRTy Ty
-  | CodeRTy
+-- -- | Calculates the greatest lower bound of two types (denoted by the meet”
+-- -- operator ⊓).
+-- meet :: Ty -> Ty -> Ty
+-- meet  = undefined -- FIXME
