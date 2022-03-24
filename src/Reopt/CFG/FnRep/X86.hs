7ebee20bd20207c4c07ed0ef1caad5d5e86df8e5
Simon Winwood <simonjwinwood@gmail.com>
2022-03-24 00:41:14 -0700

    Update reopt for latest Maxaw
    Note that the changes to X86Syscall might need to be revisited in macaw

--- src/Reopt/CFG/FnRep/X86.hs
+++ src/Reopt/CFG/FnRep/X86.hs
@@ -12,38 +12,20 @@ module Reopt.CFG.FnRep.X86
 
 
 import Data.Macaw.CFG (IsArchStmt(..))
-import Data.Macaw.Types
 import Data.Macaw.X86.ArchTypes (X86_64, X86Stmt)
-import Data.Parameterized.Some
 import Data.Parameterized.TraversableF
-import Prettyprinter
 
 import Reopt.CFG.FnRep
-
-commas :: [Doc a] -> Doc a
-commas = hsep . punctuate ","
 
 ------------------------------------------------------------------------
 -- X86ArchStmt
 
-data X86FnStmt f
-   = X86FnStmt (X86Stmt f)
-   | X86FnSystemCall !(f (BVType 64)) ![f (BVType 64)] ![Some FnReturnVar]
-     -- ^ A system call with the call number, arguments, return
-     -- variable, and block to jump to when it terminates.
-     --
-     -- TODO: See if we can migrate this to @FnStmt@.
+data X86FnStmt f = X86FnStmt (X86Stmt f)
 
 type instance FnArchStmt X86_64 = X86FnStmt
 
 instance IsArchStmt X86FnStmt where
   ppArchStmt pp (X86FnStmt s) = ppArchStmt pp s
-  ppArchStmt pp (X86FnSystemCall call_no args rets) =
-    let retDocs = viewSome pretty <$> rets
-     in parens (commas retDocs)
-            <> " := syscall " <> pp call_no <> parens (commas (pp <$> args))
 
 instance FoldableF X86FnStmt where
   foldrF f s (X86FnStmt t) = foldrF f s t
-  foldrF f s (X86FnSystemCall call_no args _rets) =
-    f call_no (foldr f s args)
