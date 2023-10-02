d97b2e651e35a3268816b6003aa4cc91b0c83342
Valentin Robert <val@galois.com>
2023-10-02 08:25:36 -0700

    fix typo in script

--- scripts/test_reopt_explore.sh
+++ scripts/test_reopt_explore.sh
@@ -3,15 +3,14 @@
 set -Eeuo pipefail
 
 if [[ -x $(which reopt-explore) ]] ; then
-  REOPT_EXLPORE=$(which reopt-explore)
+  REOPT_EXPLORE=$(which reopt-explore)
 else
-  REOPT_EXLPORE="cabal run exe:reopt-explore --"
+  REOPT_EXPLORE="cabal run exe:reopt-explore --"
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 CENTOS7_DEV_DIR="$DIR/../deps/reopt-benchmark-binaries/centos7-dev/"
-
 
 if [[ $# -lt 1 ]];
 then
@@ -27,7 +26,7 @@ shift
 case $COMMAND in
     all)
       pushd $CENTOS7_DEV_DIR
-      $REOPT_EXLPORE --lib-dir=lib64 --debug-dir=debug-lib64 llvm \
+      $REOPT_EXPLORE --lib-dir=lib64 --debug-dir=debug-lib64 llvm \
 		     --export-summary=centos7-bin-summary.txt \
 		     --export-log=centos7-bin-casts.csv \
 		     bin
@@ -35,7 +34,7 @@ case $COMMAND in
       ;;
     # all-debug-info)
     #   pushd $CENTOS7_DEV_DIR
-    #   $REOPT_EXLPORE --debug-info debug-lib64
+    #   $REOPT_EXPLORE --debug-info debug-lib64
     #   popd > /dev/null # $CENTOS7_DEV_DIR
     #   ;;
     small)
@@ -45,10 +44,10 @@ case $COMMAND in
       cp bin/mkdir test-bins/mkdir
       cp bin/curl test-bins/curl
       cp bin/date test-bins/date
-      $REOPT_EXLPORE --lib-dir=lib64 --debug-dir=debug-lib64 llvm test-bins/mkdir # --export-summary=centos7-small-bin-mkdir-summary.txt
-      $REOPT_EXLPORE --lib-dir=lib64 --debug-dir=debug-lib64 llvm test-bins # --export-summary=centos7-small-bin-summary.txt
+      $REOPT_EXPLORE --lib-dir=lib64 --debug-dir=debug-lib64 llvm test-bins/mkdir # --export-summary=centos7-small-bin-mkdir-summary.txt
+      $REOPT_EXPLORE --lib-dir=lib64 --debug-dir=debug-lib64 llvm test-bins # --export-summary=centos7-small-bin-summary.txt
       popd > /dev/null # $CENTOS7_DEV_DIR
       ;;
     # *) echo "unknown command - use 'all', 'small', or 'all-debug-info'"; exit 1;;
-    *) echo "unknown command - use 'all' or 'small'"; exit 1;;    
+    *) echo "unknown command - use 'all' or 'small'"; exit 1;;
 esac
